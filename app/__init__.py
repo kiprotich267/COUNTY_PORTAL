228bb5231318b6d6781d85859f9995881497dd6e
kiprotich267 <enockierui@gmail.com>
2025-05-30 09:10:54 +0300

    modified  app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.extensions import db, migrate
+
 from config import config
 
 def create_app():
@@ -7,8 +8,18 @@ def create_app():
     app.config.from_object(config)
     db.init_app(app)
     migrate.init_app(app, db)
-
+    
+    
+    # Register blueprints
+    # blueprints imports
+    from app.main.views import main_bp
+    from app.api.routes import api_bp
     with app.app_context():
         db.create_all()
-        
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp)
+
+    return app
+
     return app      
